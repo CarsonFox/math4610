f9a32e0aa93f2587806e2a3cc6e43020ef409e74
Carson Fox <carson.fox@aggiemail.usu.edu>
2022-10-03 22:26:05 -0600

    Secant method

--- C/lib/secant.c
+++ C/lib/secant.c
@@ -1,3 +1,20 @@
-double secant(double (*f)(double), double a, double b, double tol) {
-  return 0.0;
+#include <math.h>
+
+double secant(double (*f)(double), double x0, double x1, int max, double tol) {
+  double zero = x0;
+
+  for (int i = 0; i < max; i++) {
+    double err = fabs(f(zero));
+
+    if (err < tol)
+      return zero;
+
+    double fx0 = f(x0), fx1 = f(x1);
+    zero = x1 - fx1 * (x1 - x0) / (fx1 - fx0);
+
+    x0 = x1;
+    x1 = zero;
+  }
+
+  return 0;
 }
--- C/rootfinding.h
+++ C/rootfinding.h
@@ -4,6 +4,6 @@
 double functional_iter(double (*f)(double), double x0, int max, double tol);
 double newton(double (*f)(double), double x0, int max, double tol);
 double bisect(double (*f)(double), double a, double b, double tol);
-double secant(double (*f)(double), double a, double b, double tol);
+double secant(double (*f)(double), double a, double b, int max, double tol);
 
 #endif /* include guard */
