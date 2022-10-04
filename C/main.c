7124bb601ca858ee3c83c32b572257f07896e824
Carson Fox <carson.fox@aggiemail.usu.edu>
2022-10-03 22:56:47 -0600

    Main

--- C/main.c
+++ C/main.c
@@ -8,6 +8,11 @@ int main(void) {
     return x * exp(-x);
   }
 
+  printf("%f\n", functional_iter(f, 1/2, 1000, 1e-6));
+  printf("%f\n", newton(f, 1/2, 1000, 1e-6));
+  printf("%f\n", bisect(f, -1, 2, 1e-6));
+  printf("%f\n", secant(f, -1, 1, 1000, 1e-6));
+  printf("%f\n", hybrid_bn(f, -1, 2, 1e-3, 100, 1e-9));
   printf("%f\n", hybrid_bs(f, -1, 2, 1e-3, 100, 1e-9));
 
   return 0;
