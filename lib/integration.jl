dbb3ebcf9da1e5f46b41bebc49fb3e732de6df03
Carson Fox <carson.fox@aggiemail.usu.edu>
2022-11-19 16:14:28 -0700

    Integration module

---
+++ lib/integration.jl
@@ -0,0 +1,16 @@
+module Integration
+
+function simpson(f, a, b, h)
+    domain = a:h:b
+    
+    if !(b in domain)
+        domain = vcat(domain, b)
+    end
+    
+    @assert domain[end] == b
+    
+    pairs = zip(domain[1:end-1], domain[2:end])
+    sum((bn - an) / 6 * (f(an) + 4f((an + bn) / 2) + f(bn)) for (an, bn) in pairs)
+end
+
+end
